04c2c31e05f2b2f64f2724bb6ca391aee08c5f02
ChristyAlex23160055 <x23160055@student.ncirl.ie>
2025-07-30 00:17:36 +0100

    added the distance factor

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -3,9 +3,10 @@
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
+distance=float(input("Enter the shipping distnce in km: "))
 
 ## Calculate shipping cost
-shipping_cost = weight * rate
+shipping_cost = weight * rate*(distance*.2)
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
